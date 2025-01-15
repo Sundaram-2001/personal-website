70f0928c6cc81de5d4697341ac41805ab2a46ae3
Sundaram Saroj <sundaramsaroj@Sundarams-MacBook-Air.local>
2025-01-15 17:13:43 +0530

    added env

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -13,17 +13,17 @@
       <img src="/profile.jpg" alt="Profile picture" class="profile-pic" />
       
       <div class="button-container">
-        <a href="https://x.com/sundaramtwts" >
+        <a href={twitterLink} >
         <Button  color="blue" target="_blank" class="action-button">
           <span class="button-text">Twitter</span>
         </Button>
         </a>
-        <a href="https://github.com/Sundaram-2001" >
+        <a href={githubLink} >
         <Button  color="green" class="action-button">
           <span class="button-text">Github</span>
         </Button>
         </a>
-        <a href="https://hashnode.com/@Sundaram2001">
+        <a href={hashnodeURL}>
         <Button  color="purple" class="action-button">
           <span class="button-text">Blog</span>
         </Button>
--- svelte.config.js
+++ svelte.config.js
@@ -3,6 +3,9 @@ import adapter from '@sveltejs/adapter-vercel';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   kit: {
+    env:{
+      dir:'./'
+    },
     adapter: adapter({
       runtime: 'nodejs18.x',
     }),
